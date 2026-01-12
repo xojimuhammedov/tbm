2a8a808e0f35b956ece96466d54c9d2be4d7eadb
thequlmatov <zuxroka0104@gmail.com>
2026-01-12 13:16:51 +0500

    added type'Update' form section

--- src/pages/in & out documents/17-97 external outbound document/constants/external.outbound.constants.ts
+++ src/pages/in & out documents/17-97 external outbound document/constants/external.outbound.constants.ts
@@ -1,5 +1,5 @@
 const EXTERNAL_OUTBOUND_QUERY_KEY = "registration-doc/external-outbound";
 
 const EXTERNAL_OUTBOUND_IMPORT_API =
-  "registration-doc/external-outbound/import";
+  "api/registration-doc/external-outbound/import";
 export { EXTERNAL_OUTBOUND_QUERY_KEY, EXTERNAL_OUTBOUND_IMPORT_API };
--- src/pages/in & out documents/17-98 local inbound document/constants/local.inbound.constants.ts
+++ src/pages/in & out documents/17-98 local inbound document/constants/local.inbound.constants.ts
@@ -1,4 +1,4 @@
 const LOCAL_INBOUND_QUERY_KEY = "registration-doc/local-inbound";
 
-const LOCAL_INBOUND_IMPORT_API = "registration-doc/local-inbound/import";
+const LOCAL_INBOUND_IMPORT_API = "api/registration-doc/local-inbound/import";
 export { LOCAL_INBOUND_QUERY_KEY, LOCAL_INBOUND_IMPORT_API };
