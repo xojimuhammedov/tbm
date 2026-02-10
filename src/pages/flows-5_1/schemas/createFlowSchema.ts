2fded8a6d88b8ec1f21c11633d8edebb29245b9a
thequlmatov <zuxroka0104@gmail.com>
2026-02-10 11:24:30 +0500

    updated flow5_1 lis fields

--- src/pages/flows-5_1/schemas/createFlowSchema.ts
+++ src/pages/flows-5_1/schemas/createFlowSchema.ts
@@ -4,94 +4,46 @@ import { config } from "@/shared/utils/config.ts";
 
 export const createFlowSchema = (t: (...args: TranslationArgsType) => string) =>
     z.object({
-        flow_id: z.object({
-            _id: z.string().optional(),
-            code: z.string().nonempty(
-                t("required {{field}}", {
-                    field: t("Code"),
-                    ns: config.LANG.NS.VALIDATION,
-                }),
-            ),
-            signal_level: z.string().optional(),
-        }),
-        port_a: z.string().nonempty(
+        flow_id: z.string().optional(),
+        flow_code: z.string().nonempty(
             t("required {{field}}", {
-                field: t("Port A"),
+                field: "ID ОУТС",
                 ns: config.LANG.NS.VALIDATION,
             }),
         ),
         mux_a: z.string().nonempty(
             t("required {{field}}", {
-                field: t("MUX A"),
+                field: "Mux_A",
                 ns: config.LANG.NS.VALIDATION,
             }),
         ),
         pa: z.string().nonempty(
             t("required {{field}}", {
-                field: t("PA"),
+                field: "пА",
                 ns: config.LANG.NS.VALIDATION,
             }),
         ),
         final_ms_name: z.string().nonempty(
             t("required {{field}}", {
-                field: t("Final MS name"),
+                field: "Name_MS_final",
                 ns: config.LANG.NS.VALIDATION,
             }),
         ),
-        signal_transmission_level: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("Signal transmission level"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        au4: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("AU4"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        ts: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("TS"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        pb: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("PB"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        transit: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("Transit"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        mux_b: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("MUX B"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        port_b: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("Port B"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
-        speed: z.string().nonempty(
-            t("required {{field}}", {
-                field: t("Speed"),
-                ns: config.LANG.NS.VALIDATION,
-            }),
-        ),
+        signal_transmission_level: z.string().optional(),
+        au4: z.string().optional(),
+        ts: z.string().optional(),
+        pb: z.string().optional(),
+        mux_b: z.string().optional(),
+        speed: z.string().optional(),
         column1: z.string().optional(),
         outs_id: z.string().optional(),
         international: z.string().optional(),
         forward: z.string().optional(),
         reverse: z.string().optional(),
         start: z.string().optional(),
+        port_a: z.string().optional(),
+        transit: z.string().optional(),
+        port_b: z.string().optional(),
         end: z.string().optional(),
         consumer: z.string().optional(),
         order_number: z.string().optional(),
